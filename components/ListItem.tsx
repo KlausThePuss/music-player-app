6f200e90e4a16315c5cd98c45055b5a298c2a078
Klaus <klausthepuss@gmail.com>
2023-07-20 03:32:41 -0300

    first commit

---
+++ components/ListItem.tsx
@@ -0,0 +1,10 @@
+const ListItem = () => {
+
+return (
+  <div>
+    List Item!
+  </div>
+)
+};
+
+export default ListItem;
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,56 @@
+import { IconType } from "react-icons";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+
+interface SidebarProps {
+  icon: IconType;
+  label: string;
+  active: boolean;
+  href: string;
+}
+
+const SidebarItem: React.FC<SidebarProps> = ({
+  icon: ICon,
+  label,
+  active,
+  href
+}) => {
+  
+return (
+  <Link
+    href={href}
+    className={twMerge(`
+      flex
+      flex-row
+      h-auto
+      items-center
+      w-full
+      gap-x-4
+      text-md
+      font-medium
+      cursor-pointer
+      hover:text-white
+      transition
+      text-neutral-400
+      py-1                
+      `,
+        active && "text-white"
+    )}
+  >
+    <ICon
+      size={25}
+    />
+       <p
+      className="
+        truncate
+        v-100
+        "
+    >
+      {label}
+    </p>
+  </Link>
+);
+};
+
+export default SidebarItem;
